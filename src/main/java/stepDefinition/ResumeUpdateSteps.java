1941057a83800bd8e51e18e6f3664bd6455b9fa3
Jatin ThinkPad <mail.jatint@gmail.com>
2021-06-27 22:36:10 +0530

    Changes for handling word files

--- src/main/java/stepDefinition/ResumeUpdateSteps.java
+++ src/main/java/stepDefinition/ResumeUpdateSteps.java
@@ -1,5 +1,13 @@
 package stepDefinition;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import common.Utilities;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,8 +15,15 @@ import io.cucumber.java.en.When;
 public class ResumeUpdateSteps {
 
 	@Given("The user has the resume in the necessary directory")
-	public void the_user_has_the_resume_in_the_necessary_directory() {
-		XSSFWorkbook bk;
+	public void the_user_has_the_resume_in_the_necessary_directory() throws Exception {
+		
+		String filepath = System.getProperty("user.dir") + "/src/test/resources/Resume/" + Utilities.getProperty("resume_file_name");
+		try {
+			XWPFDocument document = new XWPFDocument(new FileInputStream(new File(filepath)));
+		} 
+		catch (Exception e) {
+			throw new Exception ("File not present at " + filepath);
+		}
 	}
 	
 	@Given("The user has provided his original joining date")
